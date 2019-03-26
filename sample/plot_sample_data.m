3b17daed649b31ed08a0c07ef59f368858823290
UAB\wwarr <wwarr@uab.edu>
2019-03-26 16:57:17 -0500

    cleanup

--- sample/plot_sample_data.m
+++ sample/plot_sample_data.m
@@ -10,10 +10,12 @@ results = load( full_path );
 data = results.(name);
 
 data.Properties.UserData.Name = name;
-data.Properties.UserData.ObjectiveVariablesPath = which( data.Properties.UserData.ObjectiveVariablesPath );
-data.Properties.UserData.StlPath = which( data.Properties.UserData.StlPath );
-data.Properties.UserData.OptionsPath = which( data.Properties.UserData.OptionsPath );
-%data.draft_metric( data.draft_metric > 0 ) = - ( 1 ./ log10( data.draft_metric( data.draft_metric > 0 ) ) );
+data.Properties.UserData.ObjectiveVariablesPath = ...
+    which( data.Properties.UserData.ObjectiveVariablesPath );
+data.Properties.UserData.StlPath = ...
+    which( data.Properties.UserData.StlPath );
+data.Properties.UserData.OptionsPath = ...
+    which( data.Properties.UserData.OptionsPath );
 
 %% construct response data
 figure_resolution_px = 600;
@@ -27,12 +29,20 @@ response_data = ResponseData( data_extractor );
 %% create visualization generator
 c = Component.load_obj( fullfile( path, [ name '_' Component.NAME '.mat' ] ) );
 f = Feeders.load_obj( fullfile( path, [ name '_' Feeders.NAME '.mat' ] ) );
-vg = VisualizationGenerator( c, f );
+visualization_generator = VisualizationGenerator( c, f );
 
 %% start tool
-usrp = UnitSphereResponsePlot( response_data, vg, figure_resolution_px );
-color_map = interp1( [ 0; 1 ], repmat( [ 0.3; 0.9 ], [ 1 3 ] ), linspace( 0, 1, 256 ) );
-usrp.set_color_map( color_map );
+usrp = UnitSphereResponsePlot( ...
+    response_data, ...
+    visualization_generator, ...
+    figure_resolution_px ...
+    );
+grayscale_color_map = interp1( ...
+    [ 0; 1 ], ...
+    repmat( [ 0.3; 0.9 ], [ 1 3 ] ), ...
+    linspace( 0, 1, 256 ) ...
+    );
+usrp.set_color_map( grayscale_color_map );
 grid_color = [ 0 0 0 ];
 usrp.set_grid_color( grid_color );
 bg_color = [ 1 1 1 ];
