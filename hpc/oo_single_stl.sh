18ddcfeffa0783072fa07ea13804561f9af5a002
UAB\wwarr <wwarr@uab.edu>
2019-05-13 14:49:25 -0500

    renamed

--- hpc/oo_single_stl.sh
+++ hpc/oo_single_stl.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 ROOT_DIR=$(dirname "$(realpath "$0")")
 PATH=$PATH:"$ROOT_DIR"
+COMPONENT_NAME=$1
 
 REPO_DIR=$ROOT_DIR'/repos'
-OPT_DIR=$REPO_DIR'/casting_orientation_optimization'
-COMPONENT_NAME=$1
+OO_DIR=$REPO_DIR'/casting_orientation_optimization'
+RES_DIR=$OO_DIR'/res'
 COMPONENT_PATH=$ROOT_DIR'/oo_component_data/'$COMPONENT_NAME'_Component.mat'
 FEEDERS_PATH=$ROOT_DIR'/oo_component_data/'$COMPONENT_NAME'_Feeders.mat'
 OPTIONS_NAME='oo_options.json'
-OPTIONS_PATH=$OPT_DIR'/'$OPTIONS_NAME
+OPTIONS_PATH=$RES_DIR'/'$OPTIONS_NAME
 OBJECTIVE_NAME='objective_variables.json'
-OBJECTIVE_PATH=$OPT_DIR'/'$OBJECTIVE_NAME
-OUTPUT_PATH=$ROOT_DIR'/results'
+OBJECTIVE_PATH=$RES_DIR'/'$OBJECTIVE_NAME
+TIME=$(date +%s%N)
+OUTPUT_BASE_PATH=$ROOT_DIR'/oo_results/'$COMPONENT_NAME
+OUTPUT_PATH=$OUTPUT_BASE_PATH'_'$TIME
+mkdir -p $OUTPUT_PATH
 RUN_CMD='generate_csvs_on_hpc( '\'$COMPONENT_PATH\'', '\'$FEEDERS_PATH\'', '\'$OPTIONS_PATH\'', '\'$OBJECTIVE_PATH\'', [$ANGLES], $SLURM_ARRAY_TASK_ID, $SLURM_JOB_ID, '\'$OUTPUT_PATH\'' );'
 FULL_CMD=$( create_matlab_command -a -c $REPO_DIR -d $ROOT_DIR -f "$RUN_CMD" )
 
@@ -28,10 +32,10 @@ MAILTYPE=FAIL
 MAILADDRESS='wwarr@uab.edu'
 
 module load rc/matlab/R2018a
-JOB_ID=$( sbatch --array=0-0%$MAXTASKS --job-name=$NAME --output=output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
+JOB_ID=$( sbatch --array=0-$ARRAYMAX%$MAXTASKS --job-name=$NAME --output=output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
 #!/bin/bash
 ANGLES=\$( sed \$(( \$SLURM_ARRAY_TASK_ID+1 ))'q;d' $CSV_PATH )
-matlab -nodisplay -nodesktop -sd $ROOT_DIR -r $RUN_CMD
+matlab -nodisplay -nodesktop -sd $ROOT_DIR -r $FULL_CMD
 LIMITING_STRING
 )
 JOB_ID=${JOB_ID##* }
