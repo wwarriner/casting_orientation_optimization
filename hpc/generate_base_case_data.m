bed6dea8567c10256ee64ba1ebdb20feba2a270c
UAB\wwarr <wwarr@uab.edu>
2019-05-30 19:55:19 -0500

    updated to be in line with v0.2 of toolsuite

--- hpc/generate_base_case_data.m
+++ hpc/generate_base_case_data.m
@@ -1,31 +1,33 @@
 function [ c_path, f_path ] = generate_base_case_data( ...
     option_path, ...
-    stl_path, ...
+    stl_file, ...
     output_folder ...
     )
 
-options = Options( '', option_path, stl_path, output_folder );
+options = Options( option_path );
+options.set( 'manager.stl_file', stl_file );
+options.set( 'manager.output_folder', output_folder );
 try
     pm = ProcessManager( options );
     pm.run();
 catch e
     fprintf( 1, '%s\n', getReport( e ) );
-    fprintf( 1, '%s\n', options.input_stl_path );
+    fprintf( 1, '%s\n', options.get( 'manager.stl_file' ) );
     assert( false );
 end
 
-if ~isfolder( options.output_path )
-    mkdir( options.output_path )
+if ~isfolder( options.get( 'manager.output_folder' ) )
+    mkdir( options.get( 'manager.output_folder' ) )
 end
 
 component_pk = ProcessKey( Component.NAME );
 c = pm.results.get( component_pk );
-c_path = fullfile( options.output_path, [ c.name '_' Component.NAME '.mat' ] );
+c_path = fullfile( options.get( 'manager.output_folder' ), [ c.name '_' Component.NAME '.mat' ] );
 c.save_obj( c_path );
 
 feeders_pk = ProcessKey( Feeders.NAME );
 f = pm.results.get( feeders_pk );
-f_path = fullfile( options.output_path, [ c.name '_' Feeders.NAME '.mat' ] );
+f_path = fullfile( options.get( 'manager.output_folder' ), [ c.name '_' Feeders.NAME '.mat' ] );
 f.save_obj( f_path );
 
 end
