44ed74241a553ea0397c001302829ce80bba8083
William Warriner <wwarriner@gmail.com>
2019-08-27 16:22:23 -0500

    updated data extraction files

--- src/data_extraction/oo_base_cases.sh
+++ src/data_extraction/oo_base_cases.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
-ROOT_DIR=$(dirname "$(realpath "$0")")
+SHELL_DIR=$(dirname "$(realpath "$0")")
+ROOT_DIR=$(realpath "$SHELL_DIR/../../../..")
 PATH=$PATH:"$ROOT_DIR"
 
 # get array of all stls in folder
-STL_PATH=$ROOT_DIR/oo_stl
+STL_FOLDER_PATH=$ROOT_DIR'/ict_stl'
 shopt -s nocaseglob
 STL_PATHS=()
-for filename in $STL_PATH/*.stl; do
+for filename in $STL_FOLDER_PATH/*.stl; do
+    echo "$filename"
 	STL_PATHS+=("$filename")
 done
 STL_PATH_COUNT=${#STL_PATHS[@]}
 
-REPO_DIR=$ROOT_DIR'/repos'
-OUTPUT_DIR=$ROOT_DIR'/oo_component_data'
-OPTION_DIR=$REPO_DIR'/casting_orientation_optimization/res/oo_options.json'
-RUN_CMD='generate_base_case_data( '\'$OPTION_DIR\'', '\''$STL_PATH'\'', '\'$OUTPUT_DIR\'' );'
-FULL_CMD=$( create_matlab_command -a -c $REPO_DIR -d $ROOT_DIR -f "$RUN_CMD" )
-printf "%s\n" "$FULL_CMD"
+
+REPOS_DIR=$ROOT_DIR'/repos'
+OUTPUT_DIR=$ROOT_DIR'/oo_data'
+
+# Toolsuite
+CGT_DIR=$REPOS_DIR'/casting_geometric_toolsuite'
+
+# Orientation Optimization Analysis
+COO_DIR=$REPOS_DIR'/casting_orientation_optimization'
+SETTINGS_FILE=$COO_DIR'/src/data_extraction/extraction_settings.json'
+
+# MATLAB
+RUN_CMD='addpath( genpath( '\'$CGT_DIR\'' ) );addpath( genpath( '\'$COO_DIR\'' ) );generate_base_case_data( '\'$SETTINGS_FILE\'', '\''$STL_PATH'\'', '\'$OUTPUT_DIR\'' );exit;'
+printf "%s\n" $FULL_CMD
 
 NAME=oo_project
 ARRAYMAX=$(( array_max=$STL_PATH_COUNT-1 ))
@@ -25,14 +35,16 @@ TASKS=1
 MEMORY='20GB'
 TIME=2:00:00
 PARTITION=express
+PARTITION=short
 MAILTYPE=FAIL
 MAILADDRESS='wwarr@uab.edu'
 
-module load rc/matlab/R2018a
-sbatch --array=0-$ARRAYMAX%$MAXTASKS --job-name $NAME --output=output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
+#0-$ARRAYMAX
+sbatch --array=0-$ARRAYMAX%$MAXTASKS --job-name $NAME --output=$ROOT_DIR/output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
 #!/bin/bash
+module load rc/matlab/R2019a
 PATHS=(${STL_PATHS[@]})
 STL_PATH=\${PATHS[\$SLURM_ARRAY_TASK_ID]}
 echo "\$STL_PATH"
-matlab -nodisplay -nodesktop -nojvm -sd $ROOT_DIR -r $FULL_CMD
+matlab -nodesktop -nodisplay -sd "$REPOS_DIR" -r "$RUN_CMD"
 LIMITING_STRING
--- src/data_extraction/oo_single_stl.sh
+++ src/data_extraction/oo_single_stl.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
-ROOT_DIR=$(dirname "$(realpath "$0")")
+SHELL_DIR=$(dirname "$(realpath "$0")")
+ROOT_DIR=$(realpath "$SHELL_DIR/../../../..")
 PATH=$PATH:"$ROOT_DIR"
 COMPONENT_NAME=$1
 
-REPO_DIR=$ROOT_DIR'/repos'
-OO_DIR=$REPO_DIR'/casting_orientation_optimization'
-RES_DIR=$OO_DIR'/res'
-COMPONENT_PATH=$ROOT_DIR'/oo_component_data/'$COMPONENT_NAME'_Component.mat'
-FEEDERS_PATH=$ROOT_DIR'/oo_component_data/'$COMPONENT_NAME'_Feeders.mat'
-OPTIONS_NAME='oo_options.json'
-OPTIONS_PATH=$RES_DIR'/'$OPTIONS_NAME
-OBJECTIVE_NAME='objective_variables.json'
-OBJECTIVE_PATH=$RES_DIR'/'$OBJECTIVE_NAME
+REPOS_DIR=$ROOT_DIR'/repos'
+OUTPUT_DIR=$ROOT_DIR'/results'
+
+# Toolsuite
+CGT_DIR=$REPOS_DIR'/casting_geometric_toolsuite'
+
+# Orientation Optimization Analysis
+COO_DIR=$REPOS_DIR'/casting_orientation_optimization'
+SETTINGS_FILE=$COO_DIR'/src/data_extraction/extraction_settings.json'
+
+BASE_CASE_PATH=$ROOT_DIR'/oo_component_data/'$COMPONENT_NAME'_base_case.mat'
+
 TIME=$(date +%s%N)
 OUTPUT_BASE_PATH=$ROOT_DIR'/oo_results/'$COMPONENT_NAME
 OUTPUT_PATH=$OUTPUT_BASE_PATH'_'$TIME
 mkdir -p $OUTPUT_PATH
-RUN_CMD='generate_csvs_on_hpc( '\'$COMPONENT_PATH\'', '\'$FEEDERS_PATH\'', '\'$OPTIONS_PATH\'', '\'$OBJECTIVE_PATH\'', [$ANGLES], $SLURM_ARRAY_TASK_ID, $SLURM_ARRAY_JOB_ID, '\'$OUTPUT_PATH\'' );'
-FULL_CMD=$( create_matlab_command -a -c $REPO_DIR -d $ROOT_DIR -f "$RUN_CMD" )
+
+# MATLAB
+RECIPE_CLASS='DefaultRecipe'
+RUN_CMD='addpath( genpath( '\'$CGT_DIR\'' ) );addpath( genpath( '\'$COO_DIR\'' ) );generate_data_on_hpc( '\'$BASE_CASE_PATH\'', '\'$RECIPE_CLASS\'', [$ANGLES], $SLURM_ARRAY_TASK_ID, $SLURM_ARRAY_JOB_ID, '\'$OUTPUT_PATH\'' );exit;'
+printf "%s\n" $FULL_CMD
 
 CSV_NAME='sphere_angles.csv'
 CSV_PATH=$ROOT_DIR'/'$CSV_NAME
 NAME=oo_project
 ARRAYMAX=$(( $( wc -l < $CSV_PATH ) -1 ))
-MAXTASKS=1
+MAXTASKS=256
 TASKS=1
 MEMORY='20GB'
 TIME=2:00:00
@@ -31,31 +38,11 @@ PARTITION=express
 MAILTYPE=FAIL
 MAILADDRESS='wwarr@uab.edu'
 
-JOB_ID=$( sbatch --array=0-$ARRAYMAX%$MAXTASKS --job-name=$NAME --output=output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
+#0-$ARRAYMAX
+sbatch --array=0-$ARRAYMAX%$MAXTASKS --job-name $NAME --output=$ROOT_DIR/output/output_%A_%a.txt --ntasks=$TASKS --mem-per-cpu=$MEMORY --time=$TIME --partition=$PARTITION --mail-type=$MAILTYPE --mail-user=$MAILADDRESS <<LIMITING_STRING
 #!/bin/bash
-module load rc/matlab/R2018a
+module load rc/matlab/R2019a
 ANGLES=\$( sed \$(( \$SLURM_ARRAY_TASK_ID+1 ))'q;d' $CSV_PATH )
-matlab -nodisplay -nodesktop -sd $ROOT_DIR -r $FULL_CMD
+echo "\$ANGLES"
+matlab -nodesktop -nodisplay -sd "$REPOS_DIR" -r "$RUN_CMD"
 LIMITING_STRING
-)
-JOB_ID=${JOB_ID##* }
-printf '%s\n' "$JOB_ID"
-
-copy_file()
-{
-	DIR=`dirname "$1"`
-	BASENAME=`basename "$1"`
-	EXT=".${BASENAME##*.}"
-	NAME="${BASENAME%.*}"
-	
-	OUT_DIR="$2"
-	JOB_ID="$3"
-	CP_PATH="$OUT_DIR"'/'"$NAME"'_'"$JOB_ID""$EXT"
-	cp -fp "$1" "$CP_PATH"
-}
-
-copy_file "$OBJECTIVE_PATH" "$OUTPUT_PATH" "$JOB_ID"
-copy_file "$COMPONENT_PATH" "$OUTPUT_PATH" "$JOB_ID"
-copy_file "$FEEDERS_PATH" "$OUTPUT_PATH" "$JOB_ID"
-copy_file "$CSV_PATH" "$OUTPUT_PATH" "$JOB_ID"
-copy_file "$OPTIONS_PATH" "$OUTPUT_PATH" "$JOB_ID"
