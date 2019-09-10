0d123a610216d57fc460d1bec2010264b928657f
UAB\wwarr <wwarr@uab.edu>
2019-09-10 16:25:54 -0500

    added error checking to shell scripts

--- src/data_extraction/oo_base_cases.sh
+++ src/data_extraction/oo_base_cases.sh
@@ -3,25 +3,45 @@ SHELL_DIR=$(dirname "$(realpath "$0")")
 ROOT_DIR=$(realpath "$SHELL_DIR/../../../..")
 PATH=$PATH:"$ROOT_DIR"
 
-# get array of all stls in folder
-STL_FOLDER_PATH=$ROOT_DIR'/oo_stl'
+STL_FOLDER=$ROOT_DIR'/oo_stl'
+if [ ! -d "$STL_FOLDER" ]; then
+	printf "Can't locate STL_FOLDER: %s\n" $STL_FOLDER
+	exit
+fi
 shopt -s nocaseglob
 STL_PATHS=()
-for filename in $STL_FOLDER_PATH/*.stl; do
+for filename in $STL_FOLDER/*.stl; do
     printf "%s\n" $filename
 	STL_PATHS+=("$filename")
 done
 STL_PATH_COUNT=${#STL_PATHS[@]}
+if (( $STL_PATH_COUNT <= 0 ))
+	printf "Didn't find any STL files in %s\n" $STL_FOLDER
+	exit
+fi
 
 REPOS_DIR=$ROOT_DIR'/repos'
 OUTPUT_DIR=$ROOT_DIR'/oo_data'
+mkdir -p $OUTPUT_DIR
 
 # Toolsuite
 CGT_DIR=$REPOS_DIR'/casting_geometric_toolsuite'
+if [ ! -d "$CGT_DIR" ]; then
+	printf "Can't locate CGT_DIR: %s\n" $CGT_DIR
+	exit
+fi
 
 # Orientation Optimization Analysis
 COO_DIR=$REPOS_DIR'/casting_orientation_optimization'
+if [ ! -d "$COO_DIR" ]; then
+	printf "Can't locate COO_DIR: %s\n" $COO_DIR
+	exit
+fi
 SETTINGS_FILE=$COO_DIR'/src/data_extraction/extraction_settings.json'
+if [ ! -f "$SETTINGS_FILE" ]; then
+	printf "Can't locate COO extraction settings JSON file: %s\n" $SETTINGS_FILE
+	exit
+fi
 
 # MATLAB
 RUN_CMD='addpath( genpath( '\'$CGT_DIR\'' ) );addpath( genpath( '\'$COO_DIR\'' ) );generate_base_case_data( '\'$SETTINGS_FILE\'', '\''$STL_PATH'\'', '\'$OUTPUT_DIR\'' );exit;'
--- src/data_extraction/oo_single_stl.sh
+++ src/data_extraction/oo_single_stl.sh
@@ -9,15 +9,37 @@ OUTPUT_DIR=$ROOT_DIR'/oo_results'
 
 # Toolsuite
 CGT_DIR=$REPOS_DIR'/casting_geometric_toolsuite'
+if [ ! -d "$CGT_DIR" ]; then
+	printf "Can't locate CGT_DIR: %s\n" $CGT_DIR
+	exit
+fi
 
 # Orientation Optimization Analysis
 COO_DIR=$REPOS_DIR'/casting_orientation_optimization'
+if [ ! -d "$COO_DIR" ]; then
+	printf "Can't locate COO_DIR: %s\n" $COO_DIR
+	exit
+fi
 DATA_EXTRACTION_DIR=$COO_DIR'/src/data_extraction'
 SETTINGS_FILE=$DATA_EXTRACTION_DIR'/extraction_settings.json'
+if [ ! -f "$SETTINGS_FILE" ]; then
+	printf "Can't locate COO extraction settings JSON file: %s\n" $SETTINGS_FILE
+	exit
+fi
 ANGLES_FILE=$DATA_EXTRACTION_DIR'/sphere_angles.csv'
+if [ ! -f "$ANGLES_FILE" ]; then
+	printf "Can't locate COO angles CSV file: %s\n" $ANGLES_FILE
+	printf "Run sphere_angles.m in MATLAB to generate.\n"
+	exit
+fi
 ANGLES_COUNT=$(( $( wc -l < $ANGLES_FILE ) -1 ))
 
-BASE_CASE_PATH=$ROOT_DIR'/oo_data/'$COMPONENT_NAME'_base_case.mat'
+BASE_CASE_FILE=$ROOT_DIR'/oo_data/'$COMPONENT_NAME'_base_case.mat'
+if [ ! -f "$BASE_CASE_FILE" ]; then
+	printf "Can't locate base case MAT file: %s\n" $BASE_CASE_FILE
+	printf "Run oo_base_cases.sh first to generate.\n"
+	exit
+fi
 
 TIME=$(date +%s%N)
 OUTPUT_BASE_PATH=$ROOT_DIR'/oo_results/'$COMPONENT_NAME
@@ -26,7 +48,12 @@ mkdir -p $OUTPUT_PATH
 
 # MATLAB
 RECIPE_CLASS='DefaultRecipe'
-RUN_CMD='addpath( genpath( '\'$CGT_DIR\'' ) );addpath( genpath( '\'$COO_DIR\'' ) );generate_data_on_hpc( '\'$BASE_CASE_PATH\'', '\'$RECIPE_CLASS\'', [$ANGLES], $SLURM_ARRAY_TASK_ID, $SLURM_ARRAY_JOB_ID, '\'$OUTPUT_PATH\'' );exit;'
+RECIPE_CLASS_FILE=$DATA_EXTRACTION_DIR'\'$RECIPE_CLASS'.m'
+if [ ! -f "$RECIPE_CLASS_FILE" ]; then
+	printf "Can't locate recipe M file: %s\n" $RECIPE_CLASS_FILE
+	exit
+fi
+RUN_CMD='addpath( genpath( '\'$CGT_DIR\'' ) );addpath( genpath( '\'$COO_DIR\'' ) );generate_data_on_hpc( '\'$BASE_CASE_FILE\'', '\'$RECIPE_CLASS\'', [$ANGLES], $SLURM_ARRAY_TASK_ID, $SLURM_ARRAY_JOB_ID, '\'$OUTPUT_PATH\'' );exit;'
 
 NAME=oo_project
 ARRAYMAX=$ANGLES_COUNT
