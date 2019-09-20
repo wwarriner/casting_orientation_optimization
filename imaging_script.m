fe3e293c7f2eea7fc356475d7bab519926101c96
William Warriner <wwarriner@gmail.com>
2019-09-19 22:14:11 -0500

    improved imaging script

--- imaging_script.m
+++ imaging_script.m
@@ -1,43 +1,28 @@
 %% SETUP
 oo_data_folder = "C:\Users\wwarr\Desktop\data";
 contents = get_contents( oo_data_folder );
-contents = get_files_with_extension( contents, ".mat" );
-files = string( get_full_paths( contents ) );
-
-oo_results_folder = "C:\Users\wwarr\Desktop\results";
-contents = get_contents( oo_results_folder );
-folders = get_full_paths( contents );
-subfolders = contents.name;
+contents = get_files_with_extension( contents, ".ood" );
+ood_files = string( get_full_paths( contents ) );
 
 RESOLUTION = 300;
 OUTPUT_FOLDER = "C:\Users\wwarr\Desktop\images";
 prepare_folder( OUTPUT_FOLDER );
 
 %% LOAD
-for i = 1 : numel( files )
+for i = 1 : numel( ood_files )
     
-    file = files( i );
-    [ ~, name, ext ] = fileparts( file );
-    name = replace( name, "_base_case", "" );
-    folder_index = find( startsWith( subfolders, name ) );
-    folder = folders( folder_index );
-    assert( numel( folder ) <= 1 );
-    if numel( folder ) == 0
-        continue;
-    end
-    ood_file = fullfile( folder, name + ".ood" );
+    ood_file = ood_files( i );
     ood = OrientationData.load_obj( ood_file );
     gd = GriddedData( ood, RESOLUTION );
     
+    [ ~, name ] = fileparts( ood_file );
     output_folder = fullfile( OUTPUT_FOLDER, name );
     prepare_folder( output_folder );
     
     for j = 1 : ood.objective_count
         tag = ood.objective_tags( j );
         im = gd.get_values( tag );
-        min( im, [], "all" )
-        max( im, [], "all" )
-        im = 1 - rescale( im, 0, 1 );
+        im = 1 - rescale( im, 0.2, 0.8 );
         imwrite( im, fullfile( output_folder, tag + ".png" ) );
     end
     
