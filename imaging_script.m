771795acda35deb5b2a6c4cf50fa5ee8f02c31ad
UAB\wwarr <wwarr@uab.edu>
2019-09-24 15:04:42 -0500

    updated imaging script to produce table of extreme (min,max) values

--- imaging_script.m
+++ imaging_script.m
@@ -9,6 +9,7 @@ OUTPUT_FOLDER = "C:\Users\wwarr\Desktop\images";
 prepare_folder( OUTPUT_FOLDER );
 
 %% LOAD
+extremes = nan( ood.objective_count, 2 * numel( ood_files ) );
 for i = 1 : numel( ood_files )
     
     ood_file = ood_files( i );
@@ -22,8 +23,12 @@ for i = 1 : numel( ood_files )
     for j = 1 : ood.objective_count
         tag = ood.objective_tags( j );
         im = gd.get_values( tag );
+        extremes( j, 2 * i - 1 : 2 * i ) = [ min( im, [], "all" ) max( im, [], "all" ) ];
         im = 1 - rescale( im, 0.2, 0.8 );
         imwrite( im, fullfile( output_folder, tag + ".png" ) );
     end
     
 end
+
+assert( ~any( isnan( extremes ), "all" ) );
+csvwrite( fullfile( OUTPUT_FOLDER, "extremes.csv" ), extremes );
